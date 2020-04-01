c49ac105d6433c35c4e56bad8233811aaca3ba18
Cuong Dang <cuongdq.qa@gmail.com>
2020-04-01 15:28:03 +0700

    .

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template, request
 
 
@@ -11,17 +12,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def main():
     return render_template('index.html')
 
-@app.route('/showDemo')
-def showSignUp():
-   return render_template('demo.html')
+@app.route('/showEncode')
+def showEncode():
+   return render_template('encode.html')
 
-@app.route('/uploadAudio', methods = ['POST'])  
-def uploadAudio():  
+@app.route('/showDecode')
+def showDecode():
+   return render_template('decode.html')
+
+@app.route('/handleEncode', methods = ['POST'])  
+def handleEncode():  
     if request.method == 'POST':  
         audioFile = request.files['file']  
         secretMsg = request.form['secret']
         print(audioFile)
         print(secretMsg)
-        return render_template("demo.html")  
+        return json.dumps({
+            'data': 'Encoding successfully'
+        })  
+
+@app.route('/handleDecode', methods = ['POST'])  
+def handleDecode():  
+    if request.method == 'POST':  
+        audioFile = request.files['file']  
+        print(audioFile)
+        return json.dumps({
+            'data': 'Decoding successfully'
+        })  
+
 if __name__ == "__main__":
   app.run()
